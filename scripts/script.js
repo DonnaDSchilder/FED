e7dc843f6258f3859ca7c61b84d2825b05fdc543
DonnaDSchilder <donnadschilder@gmail.com>
2025-09-25 00:30:59 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,27 @@
-// JavaScript Document
-console.log("hi");
+console.log("hellooo");
+
+
+/*hamburger menu*/
+
+var openButton = 
+document.querySelector("header>ul>li>button"); /*geeft aan over welke button het gaat*/
+
+openButton.onclick = openMenu; /*De actie van het klikken op de knop en welke functie uitgevoerd wordt*/
+
+function openMenu(){ /*Als de functie wordt uitgevoerd gebeurd er....*/
+    var deNav = document.querySelector("header nav"); 
+    deNav.classList.add("showMenu"); /*de nav krijgt de class showMenu, dit wordt dan "aangekleed" in css*/
+}
+
+
+var closeButton = document.querySelector("header>nav button");
+
+closeButton.onclick = closeMenu;
+
+function closeMenu() {
+  var deNav = document.querySelector("header nav");
+  deNav.classList.remove("showMenu");
+}
+
+console.log("openButton:", openButton);
+console.log("closeButton:", closeButton); /*goed om te checken of ze allebei uitgevoerd worden*/
